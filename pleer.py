f13d6f20fbcbe9c0dfc1c7707fdf0c74c9fe12ca
G-S2h <s-2-h@live.fr>
2015-07-11 09:36:43 +0200

    Fixed icon not being displayed.

--- pleer.py
+++ pleer.py
@@ -1,3 +1,4 @@
+# - encoding: utf8
 import rb
 
 from gi.repository import GObject, Gio, Peas, Gtk, GdkPixbuf
@@ -27,7 +28,7 @@ class Pleer(GObject.Object, Peas.Activatable):
 		db.register_entry_type(entry_type)
 		what, width, height = Gtk.icon_size_lookup(Gtk.IconSize.LARGE_TOOLBAR)
 		# iconfile = Gio.File.new_for_path(self.plugin_info.get_data_dir()+"/icon.ico")
-		iconfile = Gio.File.new_for_path(self.plugin_info.get_data_dir()+"/vk-symbolic.svg")
+		iconfile = Gio.File.new_for_path(self.plugin_info.get_module_dir()+"/vk-symbolic.svg")
 		source_group = RB.DisplayPageGroup.get_by_id("library")
 		self.source = GObject.new(PleerSource, name=_("Pleer"), shell=shell, query_model=model, plugin=self, entry_type=entry_type, icon=Gio.FileIcon.new(iconfile))
 		shell.append_display_page(self.source, source_group)
